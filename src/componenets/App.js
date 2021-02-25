a86f30e2ff3d7e315885da5b5c510fb6c9785d78
electromidz <electromidz@gmail.com>
2021-02-25 12:21:26 +0330

    check

---
+++ src/componenets/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HashRouter, Route , Link } from 'react-router-dom';
+function App() {
+
+  return (
+    <HashRouter>
+      <div className="footer-nav-area" id="footerNav">
+      <div className="container h-100 px-0">
+        <div className="suha-footer-nav h-100">
+          <ul className="h-100 d-flex align-items-center justify-content-between ps-0">
+            <li className="active"><a href="home.html"><i className="lni lni-home"></i>Home</a></li>
+            <li><a href="message.html"><i className="lni lni-life-ring"></i>Support</a></li>
+            <li><a href="cart.html"><i className="lni lni-shopping-basket"></i>Cart</a></li>
+            <li><a href="pages.html"><i className="lni lni-heart"></i>Pages</a></li>
+            <li><a href="settings.html"><i className="lni lni-cog"></i>Settings</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </HashRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+
+// =================== CSS
+import './assets/css/bootstrap.min.css';
+import './assets/css/default/lineicons.min.css';
+import './assets/css/style.css';
+
+
+//================ Components
+import App from './componenets/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<Provider><App /></Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

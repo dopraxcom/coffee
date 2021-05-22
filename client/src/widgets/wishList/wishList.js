b4da95aed354d5ea90ceb08c7a9a4bf6a8931316
electromidz <electromidz@gmail.com>
2021-05-23 01:30:54 +0430

    listGrid done!

--- client/src/widgets/wishList/wishList.js
+++ client/src/widgets/wishList/wishList.js
@@ -1,23 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from '../../actions'
 
-export class WishList extends Component {
-  render() {
-    return (
-      <div className="row g-3" dir="ltr">
-      <div className="col-12 col-md-6">
-        <div className="card weekly-product-card">
-          <div className="card-body d-flex align-items-center">
-            <div className="product-thumbnail-side"><span className="badge badge-success">Sale</span><span className="wishlist-btn"><i className="lni lni-heart"></i></span><span className="product-thumbnail d-block"><img src="img/product/10.png" alt=""/></span></div>
-            <div className="product-description"><span className="product-title d-block">Modern Red Sofa</span>
-              <p className="sale-price"><i className="lni lni-dollar"></i>$64<span>$89</span></p>
-              <div className="product-rating"><i className="lni lni-star-filled"></i>4.88 (39)</div><span className="btn btn-danger btn-sm"><i className="me-1 lni lni-cart"></i>Buy Now</span>
+class WishList extends Component {
+
+  renderList(){
+    return this.props.data.map( item => {
+      if(item.ratio > 4.5){
+        return(
+          <div className="col-12 col-md-6">
+            <div className="card weekly-product-card">
+              <div className="card-body d-flex align-items-center">
+                <div className="product-thumbnail-side"><span className="badge badge-success">Sale</span><span className="wishlist-btn"><i className="lni lni-heart"></i></span><span className="product-thumbnail d-block"><img src={`/img/${item.img}`}
+                  alt={item.title}/></span></div>
+                <div className="product-description"><span className="product-title d-block">{item.title}</span>
+                  <p className="sale-price"><i className="lni lni-dollar"></i>{item.price}</p>
+                  <div className="product-rating">
+                    <i className="lni lni-star-filled"></i>{item.ratio}
+                  </div>
+                    <span className="btn btn-danger btn-sm">
+                      <i className="me-1 lni lni-cart"></i>Buy Now
+                    </span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        );
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="row g-3" dir="ltr">{this.renderList()}</div>
     )
   }
 }
@@ -29,5 +44,5 @@ const mapStateToProps = state => {
 };
 
 
-export default connect(mapStateToProps, fetchData)(WishList)
+export default connect(mapStateToProps, { fetchData })(WishList)
 

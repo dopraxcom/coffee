c4f51038728cc75bca28e943801be6f91d5e682d
khorasany <a.khorasany@gmail.com>
2021-06-08 08:53:30 +0430

    feat: implement service layer

--- api/backend/main.go
+++ api/backend/main.go
@@ -1,1 +1,20 @@
-package backend
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home page endpoint hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10034", nil))
+}
+
+func main() {
+	handleRequests()
+}

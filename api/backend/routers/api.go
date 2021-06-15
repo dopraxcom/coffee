4da5d10d419b80a2c8d40fe7b3ab269b885679b7
khorasany <a.khorasany@gmail.com>
2021-06-15 20:07:06 +0430

    feat: complete admin registration & authentication section

--- api/backend/routers/api.go
+++ api/backend/routers/api.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"github.com/khorasany/coffee/api/backend/controllers/authentication/login"
+	"github.com/khorasany/coffee/api/backend/controllers/authentication/register"
 	"github.com/khorasany/coffee/api/backend/controllers/authentication/users"
 	"github.com/khorasany/coffee/api/backend/services"
 	"log"
@@ -10,7 +12,11 @@ import (
 func Router() {
 	r := services.RunnerICoffeeService()
 
-	r.HandleFunc("/users", users.GetAllAdminUsers).Methods("GET")
+	r.HandleFunc("/admins", users.GetAllAdminUsers).Methods("GET")
+	r.HandleFunc("/admin/delete/{id}", users.DeleteAdmin).Methods("GET")
+	r.HandleFunc("/admin/{id}", users.GetAdminInfo).Methods("GET")
+	r.HandleFunc("/admin/register", register.CreateAdminUser).Methods("POST")
+	r.HandleFunc("/admin/login", login.LoginAdmin).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":9006", r))
 }

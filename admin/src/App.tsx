418cabb9e71ce2e649ac6a90f42e8bdc47892a19
electromidz <electromidz@gmail.com>
2021-06-22 00:19:30 +0430

    working on Simple Card

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,28 +7,99 @@ import {
   Link
 } from "react-router-dom";
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, Button, IconButton  } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import {  
+  List, 
+  Divider, 
+  ListItem, 
+  ListItemIcon, 
+  ListItemText, 
+  SwipeableDrawer, 
+  Box } from '@material-ui/core';
+
 import Home   from './componenet/home/Home';
 import Store  from './componenet/store/Store';
 import Orders from './componenet/orders/Orders';
 
+const useStyle = makeStyles((theme: Theme) => 
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      fontFamily: 'Vazir',
+    },
+    login: {
+      fontFamily: 'Vazir',
+    },
+    list: {
+      width: 250,
+    },
+    font: {
+      fontFamily: 'Vazir',
+    },
+    main: {
+      marginTop: 60
+    },
+  })
+);
+
 function App() {
+  const classes = useStyle();
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">خانه</Link>
-          </li>
-          <li>
-            <Link to="/store">انبار</Link>
-          </li>
-          <li>
-            <Link to="/users">سفارشات</Link>
-          </li>
-        </ul>
-      </nav>
+      <AppBar>
+        <Toolbar>
+          <IconButton 
+          edge="start" 
+          className={classes.menuButton} 
+          color="inherit" 
+          aria-label="menu"
+          onClick={() => setOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            
+          </Typography>
+          <SwipeableDrawer
+          anchor="left"
+          open={open}
+          onClose={() =>setOpen(false)}
+          onOpen={() => {}}
+          >
+            <div>
+              <Box textAlign="center" p={2}></Box>
+              <Divider />
+              <List className={classes.list}>
+                <ListItem button component={Link} to="/" onClick={() => setOpen(false)}>
+                  <ListItemText primary={'خانه'} />
+                </ListItem>
+                <ListItem button component={Link} to="/store" onClick={() => setOpen(false)}>
+                  <ListItemText primary={'انبار'} />
+                </ListItem>
+                <ListItem button component={Link} to="/orders" onClick={() => setOpen(false)} >
+                  <ListItemText  primary={'سفارشات'} />
+                </ListItem>
+                <ListItem button onClick={() => setOpen(false)}>
+                  <ListItemText primary={'صندوق'} />
+                </ListItem>
+              </List>
+            </div>
+          </SwipeableDrawer>
+          <Button color="inherit" className={classes.login}>وردو</Button>
+        </Toolbar>
+      </AppBar>
+    <div className={classes.main}>
       <Switch>
         <Route path="/store"  component = {Store} />
         <Route path="/orders" component = {Orders} />
--- admin/src/componenet/home/Home.tsx
+++ admin/src/componenet/home/Home.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 
+import SimpleCard from '../../widgets/cards/SimpleCard';
+
+import { 
+    Typography,
+    Grid,
+    Paper,
+    makeStyles,
+    createStyles,
+    Theme
+ } from '@material-ui/core';
+
+
+ const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    backgroundPrimary: {
+        backgroundColor:'#4895ef',
+    },
+  }),
+);
 
 function Home() {
+    const classes = useStyles();
     return(
-        <div>
-            Home
+        <div className={classes.root}>
+            <Grid container > 
+                <Grid item xs={6} >
+                    <Paper className={classes.paper}>
+                        <SimpleCard
+                            title="سفارشات جاری"
+                        />
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <SimpleCard
+                            title="میزان فروش امروز"
+                        />
+                    </Paper>
+                </Grid>
+            </Grid> 
         </div>
     );
 }

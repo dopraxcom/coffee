ff155ecde7c6218485d5f026f62b3a7e1957559f
electromidz <electromidz@gmail.com>
2021-07-09 02:58:37 +0430

    N+ one problem in fetch data

--- admin/src/componenet/orders/Orders.tsx
+++ admin/src/componenet/orders/Orders.tsx
@@ -3,16 +3,11 @@ import { useDispatch , useSelector} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreator, State } from '../../state';
 
- 
-
-
 function Orders () {
 
     const dispatch = useDispatch();
     const { fetchData } = bindActionCreators(actionCreator, dispatch);
     const state = useSelector((state: State) => state.orders)
-    fetchData();
-    console.log(state)
     return(
         <div>
             <table>

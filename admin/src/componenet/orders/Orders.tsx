2d33b66f58cac703c60ae8ec4e2759cb17fe934d
electromidz <electromidz@gmail.com>
2021-07-10 23:58:50 +0430

    working on redux

--- admin/src/componenet/orders/Orders.tsx
+++ admin/src/componenet/orders/Orders.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import { useDispatch , useSelector} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreator, State } from '../../state';
+import { useEffect } from 'react';
 
 function Orders () {
 
     const dispatch = useDispatch();
     const { fetchData } = bindActionCreators(actionCreator, dispatch);
-    const state = useSelector((state: State) => state.orders)
+    const state = useSelector((state: State) => state.orders);
+
+useEffect(() => {
+    // fetchData()
+})
+
     return(
         <div>
             <table>
-                <tr>
-                    <td></td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
 };
-
 export default Orders;

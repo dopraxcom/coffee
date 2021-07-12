739818cfbc4e795fa2e5ab64f620d122eb218687
electromidz <electromidz@gmail.com>
2021-07-13 00:00:18 +0430

    need to create interface globally

--- admin/src/componenet/orders/Orders.tsx
+++ admin/src/componenet/orders/Orders.tsx
@@ -4,10 +4,37 @@ import { bindActionCreators } from 'redux';
 import { actionCreator, State } from '../../state';
 import { useEffect, useState } from 'react';
 import { DataGrid, GridRowModel } from '@material-ui/data-grid';
+import { keys } from '@material-ui/core/styles/createBreakpoints';
 
 interface ORDER {
     order: string
 }
+
+interface OrderItem {
+    id: number,
+    orderID: string,
+    order:[
+        {
+            Qty: number,
+            cartPrice: number,
+            cat: string,
+            description: string,
+            id: number,
+            img: string,
+            name: string,
+            numer: number,
+            price: number,
+            ratio: number,
+            status: string,
+            steamatetime: string,
+            title: string,
+        }
+    ]
+}
+
+let arr = [];
+let product = {} as OrderItem;
+
 function Orders () {
     const [orderID, setOrderID] = useState<ORDER>()
     const dispatch = useDispatch();
@@ -29,19 +56,20 @@ useEffect(() => {
 
 function data(response : Array<any>) {
     response.map((item, key) => {
-        setOrderID(item.orderID)
-        return {id: key, name: item.orderID}
+        setOrderID(item)
+        console.log(item.id , item.orderID)
     })
 }
+
+console.log(state)
     return(
         <div dir="">
-            {orderID}
             <div style={{ height: 250, width: '100%' }}>
                 <DataGrid
                     columns={[{ field: 'name' }]}
                     rows={
                     [
-                        
+                               
                     ] as GridRowModel[]
                     }
                 />

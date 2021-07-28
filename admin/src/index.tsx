851766450340e670ad9b2a20ea3d28beb6c588c2
electromidz <electromidz@gmail.com>
2021-07-28 20:37:36 +0430

    add Redux Dev tools to project

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { store } from './state'
 
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
